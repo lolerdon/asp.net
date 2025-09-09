a62874cb56c5bd293c2e7b912ed3618bd37eb7c4
lollerdon <sami2007tabib@gmail.com>
2025-09-09 09:21:00 +0200

    .

---
+++ KorteBroek/KorteBroek/Controllers/KorteBroek.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KorteBroek.Controllers;
+
+public class KorteBroek : Controller
+{
+
+    [HttpPost]
+    public IActionResult Weer(int temp, int rainChance, int id)
+    {
+        bool isHot;
+        bool overWrite = false;
+        if (temp >= 20 && rainChance >= 50)
+        {
+            isHot = true;
+        }
+        else
+        {
+            isHot = false;
+        }
+
+        if (id == 3)
+        {
+            overWrite = true;
+        }
+        ViewBag.IsHot = isHot;
+        ViewBag.OverWrite = overWrite;
+        return View();
+    }
+}
---
+++ KorteBroek/KorteBroek/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace KorteBroek.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
